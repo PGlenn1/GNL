45d04a913be2833594fa031438de385ff041bb7f
PGlenn1 <piriou.glenn@gmail.com>
2021-01-26 08:40:25 +0100

    find_newline

--- get_next_line.c
+++ get_next_line.c
@@ -2,42 +2,45 @@
 
 char *find_newline(char *str)
 {
-	char *str2;
+	static char *str2;
 	char *newline;
 
 	if ((newline = ft_strchr(str, '\n')))
 	{
-		str2 = newline;
+		str2 = ft_substr(str, 0, newline - str);
 	}
 	else
 		str2 = str;
-	printf("\nstr: %s\n", str2);
 	return (str2);
 }
 
-int	main()
+int	get_next_line(int fd, char **line)
 {
 	char buff[BUFFER_SIZE + 1];
+	int ret;
+
+	while ((ret = read(fd, buff, BUFFER_SIZE)))
+	{
+		buff[ret] = '\0';
+		*line = find_newline(ft_strndup(buff, BUFFER_SIZE));
+		printf("\nbuff:%s\n", buff);
+	}
+	return (0);
+}
+
+int main()
+{
+	char *str;
 	int fd;
 	int ret;
-	static char *str;
-	int count;
 
-	fd = open("prout", O_RDONLY);
 	str = NULL;
-	count = 10;
-	while ((ret = read(fd, buff, count)))
-	{
-		if (!str)
-		{
-			str = find_newline(ft_strndup(buff, count));
-		}
-		else
-		{
-			str = find_newline(ft_strjoin(str, buff));	
-		}
-		printf("\nbuff:%s\n", buff);
-		//printf("\nstr:%s\n", str);
-	}
+	fd = open("prout", O_RDONLY);
+	ret = get_next_line(fd, &str)
+	while (ret > 0);
+	if (ret == 0)
+		return (0);
+	if (ret > -1)
+		return (1);
 	return (0);
 }
