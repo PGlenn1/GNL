f55cca3a384e841d1c3cc1e09e4ed1c472d044aa
Glenn PIRIOU <gpiriou@e2r7p12.42.fr>
2021-02-01 16:59:54 +0100

    a l'aide

--- get_next_line.c
+++ get_next_line.c
@@ -1,8 +1,7 @@
 #include "get_next_line.h"
 
-char *find_newline(char *str, char **temp, int ret)
+char *find_newline(char *str, char **temp)
 {
-	(void)ret;
 	char *nl_str;
 	char *index;
 
@@ -12,12 +11,9 @@ char *find_newline(char *str, char **temp, int ret)
 	if (index)
 	{
 		nl_str = ft_substr(str, 0, index - str);
-	//	printf("ku\n");
 		if (temp) 
 		{
-	//		printf("yo\n");
 			*temp = ft_strndup(index + 1, ft_strlen(index + 1));
-	//		printf("prout\n");
 		}
 	}
 	else
@@ -36,25 +32,20 @@ char *return_newline(char *str, char **temp, int fd, char *buff)
 	if (!str)
 		return (NULL);
 	ret = 1;
-//	printf("return nl called\n");
-	while (!(nl_str = find_newline(str, temp, ret)) && ret > 0)
+	while (!(nl_str = find_newline(str, temp)) && ret > 0)
 	{
-	//	printf("while nl_str = NULL\n");
+		//	write(1, "\nwhile\n", 7);
 		ret = read(fd, buff, BUFFER_SIZE);
 		str_copy = ft_strndup(buff, ret);
 		str = ft_strjoin(str, str_copy);
 		free(str_copy);
-		//printf("ret: %d\n", ret);
 		if (ret == 0)
 		{
-			nl_str = str;
-			//write(1, "a", 1);
-			//printf("nique\n");
-			//printf("str in while |%s|\n", str);
-			//printf("nl_str in while: |%s|\n", nl_str);
+			*temp = NULL;
+			return (str);
 		}
 	}
-	//printf("nl_str returned: %s\n", nl_str);
+	//	printf("nl_str: |%s|\n", nl_str);
 	return (nl_str);
 }
 
@@ -64,28 +55,28 @@ int	get_next_line(int fd, char **line)
 	static char *temp;
 	char *str;
 	int ret;
-	static int i;
+//		static int i;
 
-	i += 1;
+//	i += 1;
 
-//	printf("\n---------\n%d\n---------\n", i);
-
-	if (fd < 0 || !line)
+//	printf("\n----------\n%d\n----------\n", i);
+	if (fd < 0 || !line || read(fd, buff, 0) < 0)
 		return (-1);
 	ret = 1;
 	if (temp)
 	{
 		*line = return_newline(temp, &temp, fd, buff);
-		if (*line == NULL)
-		{
-			ret = 0;
-		}
 	}
 	else
 	{
 		ret = read(fd, buff, BUFFER_SIZE);
 		str = ft_strndup(buff, ret);
 		*line = return_newline(str, &temp, fd, buff);
+	}
+	if (*line == NULL || **line == '\0')
+	{
+		printf("!!!!!!!!\n");
+		ret = 0;
 	}
 	if (ret > 0)
 		return (1);
@@ -95,17 +86,24 @@ int	get_next_line(int fd, char **line)
 		return (-1);
 }
 
-//int main()
-//{
-//	char *str;
-//	int fd;
-//	int ret;
-//
-//	str = NULL;
-//	fd = open("alphabet", O_RDONLY);
-//	while ((ret = get_next_line(fd, &str)))
-//	{
-//		printf("\n*line: |%s|\n\n", str);
-//	}
-//	return (0);
-//}
+int main()
+{
+	char *str;
+	int fd;
+	//int fd2;
+	int ret;
+
+	str = NULL;
+	fd = open("no_newline.txt", O_RDONLY);
+	//fd2 = open("half_marge_bottom", O_RDONLY);
+	while ((ret = get_next_line(fd, &str)))
+	{
+	//	printf("ret: %d\n", ret);
+		printf("*line: |%s|\n", str);
+	}
+	//while ((ret = get_next_line(fd2, &str)))
+	//{
+	//	printf("*line: |%s|\n", str);
+	//}
+	return (0);
+}
