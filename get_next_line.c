9795555607fe12981ca69e9dba3bcdf80f934732
PGlenn1 <piriou.glenn@gmail.com>
2021-02-02 08:35:22 +0100

    almost done

--- get_next_line.c
+++ get_next_line.c
@@ -23,61 +23,50 @@ char *find_newline(char *str, char **temp)
 	return (nl_str);
 }
 
-char *return_newline(char *str, char **temp, int fd, char *buff)
+int	get_line(char **temp, int fd, char **line)
 {
-	char *nl_str;
+	char buff[BUFFER_SIZE + 1];
+	char *str;
 	char *str_copy;
 	int ret;
 
-	if (!str)
-		return (NULL);
+//	printf("GNL\n");
+	str = NULL;
 	ret = 1;
-	while (!(nl_str = find_newline(str, temp)) && ret > 0)
+	while (!(*line = find_newline(str, temp)) && ret > 0)
 	{
-		//	write(1, "\nwhile\n", 7);
 		ret = read(fd, buff, BUFFER_SIZE);
-		str_copy = ft_strndup(buff, ret);
-		str = ft_strjoin(str, str_copy);
-		free(str_copy);
+		if (!str)
+			str = ft_strndup(buff, ret);
+		else
+		{
+			str_copy = ft_strndup(buff, ret);
+			str = ft_strjoin(str, str_copy);
+			free(str_copy);
+		}
+//		printf("ret: %d\n", ret);
 		if (ret == 0)
 		{
-			*temp = NULL;
-			return (str);
+//			printf("ret == 0\n");
+			*line = str;
+			break;
 		}
 	}
-	//	printf("nl_str: |%s|\n", nl_str);
-	return (nl_str);
+	return (ret);
 }
 
 int	get_next_line(int fd, char **line)
 {
-	char buff[BUFFER_SIZE + 1];
 	static char *temp;
-	char *str;
 	int ret;
-//		static int i;
+	//		static int i;
 
-//	i += 1;
+	//	i += 1;
 
-//	printf("\n----------\n%d\n----------\n", i);
-	if (fd < 0 || !line || read(fd, buff, 0) < 0)
+	//	printf("\n----------\n%d\n----------\n", i);
+	if (fd < 0 || !line)
 		return (-1);
-	ret = 1;
-	if (temp)
-	{
-		*line = return_newline(temp, &temp, fd, buff);
-	}
-	else
-	{
-		ret = read(fd, buff, BUFFER_SIZE);
-		str = ft_strndup(buff, ret);
-		*line = return_newline(str, &temp, fd, buff);
-	}
-	if (*line == NULL || **line == '\0')
-	{
-		printf("!!!!!!!!\n");
-		ret = 0;
-	}
+	ret = get_line(&temp, fd, line);
 	if (ret > 0)
 		return (1);
 	else if (ret == 0)
@@ -90,20 +79,21 @@ int main()
 {
 	char *str;
 	int fd;
-	//int fd2;
+//	int fd2;
 	int ret;
 
 	str = NULL;
-	fd = open("no_newline.txt", O_RDONLY);
-	//fd2 = open("half_marge_bottom", O_RDONLY);
+	fd = open("ku", O_RDONLY);
+	//	fd2 = open("half_marge_bottom", O_RDONLY);
 	while ((ret = get_next_line(fd, &str)))
 	{
-	//	printf("ret: %d\n", ret);
-		printf("*line: |%s|\n", str);
+		//	printf("ret: %d\n", ret);
+		printf("*line: |%s\n", str);
 	}
-	//while ((ret = get_next_line(fd2, &str)))
-	//{
-	//	printf("*line: |%s|\n", str);
-	//}
+	printf("*last line: |%s\n", str);
+	//	while ((ret = get_next_line(fd2, &str)))
+	//	{
+	//		printf("*line: |%s\n", str);
+	//	}
 	return (0);
 }
