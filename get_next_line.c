274faad47762d74a2fa14b126a8bf304c951b594
Glenn PIRIOU <gpiriou@e2r7p10.42.fr>
2021-02-02 17:10:41 +0100

    malloc error

--- get_next_line.c
+++ get_next_line.c
@@ -5,68 +5,87 @@ char *find_newline(char *str, char **temp)
 	char *nl_str;
 	char *index;
 
-	if (!str)
-		return (NULL);
-	index = ft_strchr(str, '\n');
-	if (index)
+	//printf("\nFL\n");
+	//printf("str:|%s|\ntemp:|%s|\n", str, *temp);
+	nl_str = NULL;
+	if ((index = ft_strchr(*temp, '\n')))
+	{
+		nl_str = ft_substr(*temp, 0, index - *temp);
+	}
+	else if ((index = ft_strchr(str, '\n')))
 	{
 		nl_str = ft_substr(str, 0, index - str);
-		if (temp) 
-		{
-			*temp = ft_strndup(index + 1, ft_strlen(index + 1));
-		}
+	}
+	if (index)
+	{
+		*temp = ft_strndup(index + 1, ft_strlen(index + 1));
 	}
 	else
 	{
-		nl_str = NULL;
+		if (str)
+			*temp = str;
 	}
+	//printf("nl_str:|%s\n", nl_str);
 	return (nl_str);
 }
 
-int	get_line(char **temp, int fd, char **line)
+int	get_line(int fd, char **temp, char **line)
 {
+	int ret;
 	char buff[BUFFER_SIZE + 1];
 	char *str;
-	char *str_copy;
-	int ret;
+	int i;
 
-//	printf("GNL\n");
+//	printf("\nGL\n");
+	i = 0;
 	str = NULL;
 	ret = 1;
-	while (!(*line = find_newline(str, temp)) && ret > 0)
+	while (!(*line = find_newline(str, temp)))
 	{
-		ret = read(fd, buff, BUFFER_SIZE);
-		if (!str)
-			str = ft_strndup(buff, ret);
-		else
-		{
-			str_copy = ft_strndup(buff, ret);
-			str = ft_strjoin(str, str_copy);
-			free(str_copy);
-		}
-//		printf("ret: %d\n", ret);
+//		printf("\ni[%d]\n", i);
+	//	i++;
 		if (ret == 0)
 		{
 //			printf("ret == 0\n");
-			*line = str;
+			if (*temp)
+			{
+				*line = *temp;
+			}
+			else
+			{
+				*line = str;
+			}
 			break;
+		}
+		if ((ret = read(fd, buff, BUFFER_SIZE)))
+		{
+//			printf("BUFF: |%s|\n", buff);
+//			printf("!temp: %s\n", *temp);
+			str = ft_strndup(buff, ret);
+			if (*temp)
+			{
+//				printf("ret = %d\n", ret);
+				str = ft_strjoin(*temp, str);
+				*temp = NULL;
+			}
 		}
 	}
 	return (ret);
 }
 
+
 int	get_next_line(int fd, char **line)
 {
 	static char *temp;
 	int ret;
-	//		static int i;
+	//static int i;
 
-	//	i += 1;
+//	i += 1;
 
-	//	printf("\n----------\n%d\n----------\n", i);
+//	printf("----------\n%d\n----------\n", i);
 	if (fd < 0 || !line)
 		return (-1);
-	ret = get_line(&temp, fd, line);
+	ret = get_line(fd, &temp, line);
 	if (ret > 0)
 		return (1);
 	else if (ret == 0)
@@ -79,21 +98,17 @@ int main()
 {
 	char *str;
 	int fd;
-//	int fd2;
+	int fd2;
 	int ret;
 
 	str = NULL;
-	fd = open("ku", O_RDONLY);
-	//	fd2 = open("half_marge_bottom", O_RDONLY);
+	fd = open("half_marge_top", O_RDONLY);
+	fd2 = open("half_marge_bottom", O_RDONLY);
 	while ((ret = get_next_line(fd, &str)))
-	{
-		//	printf("ret: %d\n", ret);
 		printf("*line: |%s\n", str);
-	}
-	printf("*last line: |%s\n", str);
-	//	while ((ret = get_next_line(fd2, &str)))
-	//	{
-	//		printf("*line: |%s\n", str);
-	//	}
+	printf("*Line: |%s\n", str);
+	while ((ret = get_next_line(fd2, &str)))
+		printf("*line: |%s\n", str);
+	printf("*Line: |%s\n", str);
 	return (0);
 }
