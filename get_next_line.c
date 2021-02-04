5baa4b0d4f1515e5e8e194261563a8f5a0322d5c
PGlenn1 <piriou.glenn@gmail.com>
2021-02-04 21:43:40 +0100

    done!

--- get_next_line.c
+++ get_next_line.c
@@ -14,8 +14,9 @@
 
 char	*find_newline(char *str, char **temp)
 {
-	char *nl_str; /// NE PAS FREE STR
+	char *nl_str;
 	char *index;
+	char *save;
 
 	nl_str = NULL;
 	if ((index = ft_strchr(*temp, '\n')))
@@ -23,14 +24,14 @@ char	*find_newline(char *str, char **temp)
 	else if ((index = ft_strchr(str, '\n')))
 		nl_str = ft_substr(str, 0, index - str);
 	if (index)
-		*temp = ft_strndup(index + 1, ft_strlen(index + 1));
-	else
 	{
-		if (str)
-		{
-			*temp = str;
-		}
+		save = *temp;
+		*temp = ft_strndup(index + 1, ft_strlen(index + 1));
+		free(save);
+		free(str);
 	}
+	else if (str)
+		*temp = str;
 	return (nl_str);
 }
 
@@ -39,15 +40,15 @@ void	check_temp(char **temp, char **line, char **str, int ret)
 	char *str2;
 
 	str2 = NULL;
-	if (ret == 0)
+	if (ret <= 0)
 		*line = *temp;
 	else
 	{
 		str2 = *str;
 		*str = ft_strjoin(*temp, str2);
 		free(str2);
+		free(*temp);
 	}
-	*temp = NULL;
 }
 
 char	*ft_strjoin(char *s1, char *s2)
@@ -71,16 +72,12 @@ int		get_line(int fd, char **temp, char **line, int ret)
 	str = NULL;
 	while (!(*line = find_newline(str, temp)))
 	{
-		if (ret == 0)
+		if (ret <= 0)
 		{
 			if (*temp)
 				check_temp(temp, line, &str, ret);
 			else
-			{
 				*line = str;
-				if (str) /// CA CHANGE RIEN, BIZARRE
-					free(str);
-			}
 			break ;
 		}
 		if ((ret = read(fd, buff, BUFFER_SIZE)))
@@ -101,35 +98,37 @@ int		get_next_line(int fd, char **line)
 
 	if (fd < 0 || !line || read(fd, buff, 0) < 0 || BUFFER_SIZE == 0)
 		return (-1);
-	ret = 1;
-	ret = get_line(fd, &temp, line, ret);
+	ret = get_line(fd, &temp, line, 1);
 	if (*line == NULL)
-	{
 		*line = ft_strndup("", 0);
-	}
 	if (ret > 0)
-	{
 		return (1);
-	}
 	else if (ret == 0)
 	{
 		free(temp);
 		return (0);
 	}
 	else
+	{
+		free(temp);
 		return (-1);
+	}
 }
 
-int main()
-{
-	char *str;
-	int fd;
-	int ret;
-
-	str = NULL;
-	fd = open("bigline.txt", O_RDONLY);
-	while ((ret = get_next_line(fd, &str)));
-		printf("*line: |%s\n", str);
-	printf("*Line: |%s\n", str);
-	return (0);
-}
+//int main()
+//{
+//	char *str;
+//	int fd;
+//	int ret;
+//
+//	str = NULL;
+//	fd = open("alphabet", O_RDONLY);
+//	while ((ret = get_next_line(fd, &str)))
+//	{
+////		printf("*line: |%s\n", str);
+//		free(str);
+//	}
+////	printf("*Line: |%s\n", str);
+//	free(str);
+//	return (0);
+//}
