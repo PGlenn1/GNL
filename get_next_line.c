d03999e213c5e9fedad8d004a62a4c008ff2ec85
Glenn PIRIOU <glenn.glpiriou@student.42.fr>
2023-02-12 21:28:30 +0100

    project normed, missing bonus files

--- get_next_line.c
+++ get_next_line.c
@@ -1,112 +1,126 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glpiriou <glpiriou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/12 20:25:03 by glpiriou          #+#    #+#             */
+/*   Updated: 2023/02/12 21:25:59 by glpiriou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-int ffd(struct fd_data *ar, int fd)
+int	ffd(struct s_fd_data *ar, int fd)
 {
-    int i;
+	int	i;
 
-    if (fd < 0)
-        return (-1);
-    i = 0;
-    while (i < ARRAY_SIZE && ar[i].fd != fd)
-    {
-        if (ar[i].fd == 0)
-        {
-            ar[i].fd = fd;
-            return (i);
-        }
-        i++;
-    }
-    if (i == ARRAY_SIZE)
-        return (-1);
-    return (i);
+	if (fd < 0)
+		return (-1);
+	if (fd == 0)
+		return (0);
+	i = 0;
+	while (i < ARRAY_SIZE && ar[i].fd != fd)
+	{
+		if (ar[i].fd == 0)
+		{
+			ar[i].fd = fd;
+			return (i);
+		}
+		i++;
+	}
+	if (i == ARRAY_SIZE)
+		return (-1);
+	return (i);
 }
 
-char *find_nl(char *str, char **save)
+char	*find_nl(char *str, char **save)
 {
-    char *tmp;
-    unsigned int index;
+	char			*tmp;
+	unsigned int	index;
 
-    tmp = str;
-    index = ft_strchr(str, '\n');
-    if (index || *str == '\n')
-    {
-        str = ft_strndup(tmp, index + 1);
-        if (!str)
-            return (NULL);
-        if (tmp[index + 1] || *tmp == '\n')
-        {
-            *save = ft_strndup(&tmp[index + 1], ft_strlen(&tmp[index + 1]));
-            if (!*save)
-                return (NULL);
-        }
-        else
-            *save = NULL;
-        free(tmp);
-    }
-    return (str);
+	tmp = str;
+	index = ft_strchr(str, '\n');
+	if (index || *str == '\n')
+	{
+		str = ft_strndup(tmp, index + 1);
+		if (!str)
+			return (NULL);
+		if (tmp[index + 1] || *tmp == '\n')
+		{
+			*save = ft_strndup(&tmp[index + 1], ft_strlen(&tmp[index + 1]));
+			if (!*save)
+				return (NULL);
+		}
+		else
+			*save = NULL;
+		free(tmp);
+	}
+	return (str);
 }
 
-char *create_line(char **save, char *buffer)
+char	*create_line(char **save, char *buffer)
 {
-    char *line;
+	char	*line;
 
-    line = NULL;
-    if (!*save || !**save)
-    {
-        if (*buffer)
-            line = ft_strjoin(buffer, "");
-    }
-    else
-    {
-        line = ft_strjoin(*save, buffer);
-        if (!line)
-            return (NULL);
-        free(*save);
-        *save = NULL;
-    }
-    return (line);
+	line = NULL;
+	if (!*save || !**save)
+	{
+		if (*buffer)
+			line = ft_strjoin(buffer, "");
+	}
+	else
+	{
+		line = ft_strjoin(*save, buffer);
+		if (!line)
+			return (NULL);
+		free(*save);
+		*save = NULL;
+	}
+	return (line);
 }
 
-char *join_line(char *line, char **save, char *buffer)
+char	*join_line(char *line, char **save, char *buffer)
 {
-    char *tmp;
+	char	*tmp;
 
-    tmp = line;
-    if (!line)
-        line = create_line(save, buffer);
-    else
-    {
-        line = ft_strjoin(tmp, buffer);
-        if (!line)
-            return (NULL);
-        free(tmp);
-    }
-    return (line);
+	tmp = line;
+	if (!line)
+		line = create_line(save, buffer);
+	else
+	{
+		line = ft_strjoin(tmp, buffer);
+		if (!line)
+			return (NULL);
+		free(tmp);
+	}
+	return (line);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    char buffer[BUFFER_SIZE + 1];
-    char check[1];
-    char *line;
-    static struct fd_data ar[ARRAY_SIZE];
-    int ret;
+	char					buffer[BUFFER_SIZE + 1];
+	char					check[1];
+	char					*line;
+	static struct s_fd_data	ar[ARRAY_SIZE];
+	int						ret;
 
-    if (fd < 0 || ffd(ar, fd) < 0 || read(fd, check, 0) < 0 || BUFFER_SIZE == 0)
-        return (NULL);
-    ret = 1;
-    line = NULL;
-    if (ft_strchr(ar[ffd(ar, fd)].save, '\n'))
-        return (find_nl(ar[ffd(ar, fd)].save, &(ar[ffd(ar, fd)].save)));
-    while (ret > 0 && !ft_strchr(line, '\n'))
-    {
-        ret = read(fd, buffer, BUFFER_SIZE);
-        if (ret < 0)
-            return (NULL);
-        buffer[ret] = '\0';
-        line = join_line(line, &(ar[ffd(ar, fd)].save), buffer);
-        if (!line)
-            return (NULL);
-    }
-    return (find_nl(line, &(ar[ffd(ar, fd)].save)));
+	if (fd < 0 || ffd(ar, fd) < 0 || read(fd, check, 0) < 0 || BUFFER_SIZE == 0)
+		return (NULL);
+	ret = 1;
+	line = NULL;
+	if (ft_strchr(ar[ffd(ar, fd)].save, '\n'))
+		return (find_nl(ar[ffd(ar, fd)].save, &(ar[ffd(ar, fd)].save)));
+	while (ret > 0 && !ft_strchr(line, '\n'))
+	{
+		ret = read(fd, buffer, BUFFER_SIZE);
+		if (ret < 0)
+			return (NULL);
+		buffer[ret] = '\0';
+		line = join_line(line, &(ar[ffd(ar, fd)].save), buffer);
+		if (!line)
+			return (NULL);
+	}
+	return (find_nl(line, &(ar[ffd(ar, fd)].save)));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,25 +1,42 @@
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <strings.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glpiriou <glpiriou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/12 20:36:20 by glpiriou          #+#    #+#             */
+/*   Updated: 2023/02/12 21:25:34 by glpiriou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 42
-#endif
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#ifndef ARRAY_SIZE
-#define ARRAY_SIZE 1024
-#endif
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <strings.h>
+# include <unistd.h>
 
-typedef struct fd_data
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10000000
+# endif
+
+# ifndef ARRAY_SIZE
+#  define ARRAY_SIZE 1024
+# endif
+
+typedef struct s_fd_data
 {
-	int fd;
-	char *save;
-} fd_data;
+	int			fd;
+	char		*save;
+}				t_fd_data;
 
-int ft_strchr(char *s, int c);
-char *ft_strjoin(char *s1, char *s2);
-unsigned int ft_strlen(char *str);
-char *ft_strndup(char *s1, unsigned int n);
-char *get_next_line(int fd);
+int				ft_strchr(char *s, int c);
+char			*ft_strjoin(char *s1, char *s2);
+unsigned int	ft_strlen(char *str);
+char			*ft_strndup(char *s1, unsigned int n);
+char			*get_next_line(int fd);
+
+#endif
