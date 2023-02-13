a0b66664e50f63fd817fe13d5f0e2348d9fa5fdc
Glenn PIRIOU <glenn.glpiriou@student.42.fr>
2023-02-13 16:54:59 +0100

    added bonus.h

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: glpiriou <glpiriou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 20:36:20 by glpiriou          #+#    #+#             */
-/*   Updated: 2023/02/13 15:16:55 by glpiriou         ###   ########.fr       */
+/*   Updated: 2023/02/13 16:54:39 by glpiriou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <fcntl.h>
 # include <stdio.h>
